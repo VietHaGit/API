8f6351bcec8971b94c40502a3667c1d529261560
VietHaGit <jimwaltongit@gmail.com>
2023-04-10 22:54:35 +0700

    commit :v

---
+++ src/main/java/com/example/demo/ElementController.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/elements")
+public class ElementController  {
+
+    @GetMapping
+    public List<String> getElements(){
+        List<String> elements = new ArrayList<>();
+        elements.add("A");
+        elements.add("B");
+        elements.add("C");
+        return elements;
+    }
+}
